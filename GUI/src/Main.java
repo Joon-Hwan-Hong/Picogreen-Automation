d795c678b2e74665c59931d59ad1a7e04f2cce80
Joon-Hwan-Hong <joon.hong@mail.mcgill.ca>
2020-01-22 12:23:59 -0500

    reorganization of buttons & functions

--- GUI/src/Main.java
+++ GUI/src/Main.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -10,10 +11,11 @@ import javafx.stage.Stage;
 import javafx.stage.FileChooser;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
-import javafx.stage.DirectoryChooser;
-
 
 import java.io.File;
+import java.util.ArrayList;
+
+import java.lang.reflect.Array;
 
 public class Main extends Application {
 
@@ -29,22 +31,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Button button1;
         Button button2;
         Button button3;
-        Button button4;
         Button button5;
+        Button button_ab1;
+        Button button_excel;
 
         window = primaryStage;
         primaryStage.setTitle("Picogreen Report Generator by Joon Hwan Hong");
 
-        DirectoryChooser directoryChooser = new DirectoryChooser();
-        // Button for data file selection section
-        button1 = new Button("Select data file");
-        button1.setOnAction(actionEvent -> { File datafile  = fileChooser.showOpenDialog(primaryStage); } );
-        // Button for settings file selection section
-        button4 = new Button("Select settings file");
-        button4.setOnAction(actionEvent -> { File settingsfile = fileChooser.showOpenDialog(primaryStage); } );
         // Button for about and help section
         button2 = new Button("github page");
         button2.setOnAction(actionEvent -> window.setScene(scene_help) );
@@ -52,18 +47,34 @@ public class Main extends Application {
         button3 = new Button("Return to data selection");
         button3.setOnAction(actionEvent -> window.setScene(scene_main));
 
-        // Button for calculation
-        button5 = new Button("Generate Report");
+        // Button for running python file
+        button5 = new Button("2. Generate Report");
         button5.setOnAction(actionEvent -> {
             // test if both data file and settings file are selected, if not pop up a string that says that
             // then run a python file using whatever method
-            // then notify that it has been successful in a different layout. and give a button to close the application
+            // then notify that it has been successful in a different layout.
         });
 
+        // Button for alertbox
+        button_ab1 = new Button("1. Select files");
+        button_ab1.setOnAction(actionEvent -> {
+                final ArrayList files = AlertBox
+                        .display("Select files and return", "Select the needed files");});
+
+        // Button for opening the excel sheet
+        button_excel = new Button("3. Open the .xlsx report");
+        button_excel.setOnAction(actionEvent -> {
+            // test if generating report has been made
+            // then use the desktop API class to open the excel file report
+            // https://stackoverflow.com/questions/17276688/open-excel-from-java-application
+        });
+
+
         // main layout
-        Label main_label = new Label("Please select a .xlsx data file to proceed");
+        Label main_label = new Label("Follow the steps to proceed.");
         VBox layout_main = new VBox(10);
-        layout_main.getChildren().addAll(main_label, button1, button4, button2);
+        layout_main.getChildren().addAll(main_label, button_ab1, button5, button_excel, button2);
+        layout_main.setAlignment(Pos.CENTER);
         scene_main = new Scene(layout_main, 450, 300);
 
         // about and help layout
